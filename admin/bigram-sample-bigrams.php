b67290d643901c8c3e1746e90d6c24c18d381d37
Herb Miller <herb@bobbingwide.com>
2018-07-04 11:32:11 +0100

    Issue #4 - batch routine to generate sampled bigrams from existing content

---
+++ admin/bigram-sample-bigrams.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @copyright (C) Copyright Bobbing Wide 2018
+ *
+ */
+ 
+ 
+if ( PHP_SAPI !== "cli" ) { 
+	die();
+}
+
+/**
+ * Extracts existing bigrams from content
+ * 
+ * Implement issue #4 in batch
+ * 
+ * 1. List all bigrams
+ * 2. Create mapping 
+ * 3. For each post in the mapping
+ * 4. sample_bigram - find sampled bigrams
+ * 5. Create each new sampled bigram
+ * 
+ */
+function bigram_sample_bigrams() {
+	oik_require( "classes/class-sample-bigrams.php", "bigram" );
+	$sample_bigrams = new sample_bigrams();
+	$sample_bigrams->load();
+	$sample_bigrams->map_posts();
+	$sample_bigrams->process();
+	$sample_bigrams->report();
+
+
+}
+
+ 
+ 
+bigram_sample_bigrams();
+
+
+
---
+++ classes/class-sample-bigrams.php
@@ -0,0 +1,303 @@
+<?php 
+
+/**
+ * @copyright (C) Copyright Bobbing Wide 2018
+ * @package bigram
+ * 
+ */
+class sample_bigrams {
+
+	/**
+	 * All bigrams 
+	 */
+	public $posts;
+	
+	/**
+	 * Mapping of bigrams to IDs
+	 * first unique key found comes first
+	 * ... is that a good idea or not?
+	 
+	 * $mapping[ $post_name ] => ID  ?
+	 */
+	public $mapping;
+	public $already_mapped;
+	public $sampled;
+	
+	
+	/**
+	 * 
+	 */
+	function __construct() {
+		oik_require( "includes/bw_posts.php" );
+		$this->mapping = array();
+		$this->already_mapped = 0;
+		$this->posts = null;
+		$this->sampled = array();
+	}
+	
+	/**
+	 * Load all the bigram posts
+	 * 
+	 * There are thousands
+	 */
+	function load() {
+		$args = array( "post_type" => "bigram" 
+								 , "numberposts" => -1
+								 , "orderby" => "date"
+								 , "order" => "asc"
+								 );
+		$posts = bw_get_posts( $args );
+		echo count( $posts ) . PHP_EOL;
+		$this->posts = $posts;
+	}
+	
+	/**
+	 * Map post titles to ID 
+	 */ 
+	function map_posts() {
+		foreach ( $this->posts as $post ) {	 
+			$this->map( $post->post_name, $post->ID );
+		}
+		echo count( $this->mapping ) . PHP_EOL;
+		echo $this->already_mapped . PHP_EOL;
+	}
+	
+	
+	/**
+	 * Maps the post_name to ID
+	 * 
+	 * Enables a quick lookup of the post name which is expected to be of the form `sword-bword`
+	 * when there's more than one then a suffix gets added 
+	 * but that's not expected to be an issue
+	 *
+	 WP_Post Object
+(
+    [ID] => 212
+    [post_author] => 1
+    [post_date] => 1991-07-30 00:00:00
+    [post_date_gmt] => 1991-07-30 00:00:00
+    [post_content] => By Jon<!--more--><br />From the original SB.txt
+    [post_title] => Business FactorTable
+    [post_excerpt] =>
+    [post_status] => publish
+    [comment_status] => closed
+    [ping_status] => closed
+    [post_password] =>
+    [post_name] => business-factortable
+    [to_ping] =>
+    [pinged] =>
+    [post_modified] => 2016-05-09 08:26:05
+    [post_modified_gmt] => 2016-05-09 08:26:05
+    [post_content_filtered] =>
+    [post_parent] => 0
+    [guid] => http://qw/bigram/bigram/business-factortable/
+    [menu_order] => 0
+    [post_type] => bigram
+    [post_mime_type] =>
+    [comment_count] => 0
+    [filter] => raw
+)
+	 */
+		
+	function map( $post_name, $ID ) {
+		if ( !isset( $this->mapping[ $post_name ] ) ) {
+			$this->mapping[ $post_name ] = $ID;
+		}	else {
+			$this->already_mapped++;
+		}
+	}
+	
+	/**
+	 * Gets the mapped ID
+	 */
+	function get_mapping( $sword, $bword ) {
+		$key = "$sword-$bword";
+		$key = sanitize_title( $key );
+		$ID = bw_array_get( $this->mapping, $key, null );
+		return $ID;
+	}
+	
+	function process() {
+		foreach ( $this->posts as $post ) {	
+			$this->sampled = array(); 
+			$this->sample( $post );
+			print_r( $this->sampled );
+			
+			foreach ( $this->sampled as $key => $sample ) {
+				$sword = $sample[ 'sword' ];
+				$bword = $sample[ 'bword' ];
+				$this->maybe_create_sampled_bigram( $post, $sword, $bword );
+			}
+		}
+	}
+	
+	/**
+	 * Converts SB's to links
+	 *
+	 * Creates a list of sampled bigrams to be checked
+	 *
+	 * If the SB is already in a link then we shouldn't need to worry about it.
+	 */
+	function sample( $post ) {
+		echo "Processing {$post->ID} {$post->post_title}" . PHP_EOL;
+		$content = $post->post_content;
+		$contents = explode( " ", $content );
+		//print_r( $contents );
+		$sword_index = null;
+		foreach ( $contents as $index => $word ) {
+			$char = strtolower( substr( $word, 0, 1 ) );
+			switch ( $char ) {
+				case 'b':
+					if ( $sword_index !== null ) {
+						$this->make_link( $contents, $sword_index );
+					}
+					$sword_index = null;
+				break;
+				case 's':
+					$sword_index = $index;
+				break;
+						
+				default:
+					$sword_index = null;
+			}
+		}
+		$this->update( $post, $contents );
+	}
+	
+	function report() {
+	
+	}
+	
+	/**
+	 * Creates a link to the SB bigram
+	 * 
+	 * Strips the bword from the second word and replaces the sword with the link
+	 * <a href="https://qw/bigram/bigram/sword-bword>sword bword</a> 
+	 * 
+	 */
+	function make_link( &$contents, $sword_index ) {
+		$sword = $this->get_sbword( $contents[ $sword_index ] );
+		$bword = $this->get_sbword( $contents[ $sword_index+1 ] );
+		$this->add_sampled( $sword, $bword );
+		$this->create_sword_link( $contents, $sword_index, $sword, $bword );
+		$this->remove_bword( $contents, $sword_index, $sword, $bword );
+	}
+	
+	function add_sampled( $sword, $bword ) {
+		echo "$sword $bword" . PHP_EOL;
+		$this->sampled["$sword $bword"] = array( 'sword' => $sword, 'bword' => $bword );
+	}
+	
+	function get_sbword( $sbwordystuff ) {
+		//echo $sbwordystuff . PHP_EOL;
+		$bwords = preg_match( "/[sSbB]([a-zA-Z'])+/", $sbwordystuff, $words );
+		//echo $bwords;
+		//print_r( $words );
+		$sbword = strtolower( $words[0] );
+		return $sbword;
+	}
+	
+	/**
+	 * Creates the sword link 
+	 * 
+	 * Replaces the existing sword with a link
+	 * 
+	 * @param array $contents the contents array
+	 * @param integer $sword_index 
+	 * @param string $sword the sanitized sword	- lower case
+	 * @param string $bword the sanitized bword - lower case ( but may contain apostrophes ? )
+	 */
+	function create_sword_link( &$contents, $sword_index, $sword, $bword ) {
+		$link_text = $contents[ $sword_index ];
+		$link_text .= " ";
+		$link_text .= substr( $contents[ $sword_index+1 ], 0, strlen( $bword ) );
+		$url = site_url( "/bigram/$sword-$bword", "https" );
+		$link = retlink( null, $url, $link_text );
+		echo $link . PHP_EOL;
+		$contents[ $sword_index ] = $link;  
+	}
+	
+	function remove_bword( &$contents, $sword_index, $sword, $bword ) {
+		$bword_index = $sword_index + 1;
+		$new_bword = $contents[ $bword_index ] ;
+		$new_bword = substr( $new_bword, strlen( $bword ) );
+		$contents[ $bword_index ] = $new_bword;
+		echo $new_bword;
+	}
+	
+	/**
+	 * Updates the post if the contents is now different
+	 * 
+	 * We have to ensure that we don't intercept save_post
+	 * and end up in a loop. 
+	 */
+	function update( $post, $contents ) {
+	
+	
+	}
+	
+	/**
+	 * Create sampled bigram 
+	 *
+	 * @param object $post
+	 * @param string $sword
+	 * @param string $bword
+	 */
+	function maybe_create_sampled_bigram( $post, $sword, $bword ) {
+		$mapped = $this->get_mapping( $sword, $bword );
+		if ( !$mapped ) { 
+			$post = $this->create_sampled_bigram( $post, $sword, $bword );
+			$this->map( $post->post_name, $post->ID );
+		}
+	}
+	
+	/**
+	 * Creates a sampled bigram
+	 *
+	 * Damn! It failed to set the Category taxonomy
+	 * 
+	 * @param object $post the original post object
+	 * @param string $sword 
+	 * @param string $bword
+	 * @return object the sampled post 
+	 */
+	function create_sampled_bigram( $post, $sword, $bword ) {
+		echo "Creating sampled bigram: $sword $bword" . PHP_EOL;
+		$content = $this->post_content( $post ); 		
+		$title = $this->post_title( $sword, $bword );
+		$sampled = array( "post_author" => $post->post_author
+										, "post_date" => $post->post_date
+										, "post_modified" => $post->post_modified
+										, "post_content" => $content
+										, "post_title" => $title
+										, "post_type" => $post->post_type
+										, "post_status" => "publish"
+										, "comment_status" => "closed"
+										, "ping_status" => "closed"
+										);
+		$ID = wp_insert_post( $sampled );
+		$sampled_post = null;
+		if ( $ID ) {
+			$sampled_post = get_post( $ID );
+		} else {
+			echo "Failed to create the sampled post" . PHP_EOL;
+			gob();
+		}
+		return $sampled_post;
+	}
+	
+	function post_content( $post ) {
+		$post_content = "<!--more-->Sampled from {$post->post_title}.";
+		return $post_content;
+	}
+
+	function post_title( $sword, $bword ) {
+		$post_title = ucfirst( $sword );
+		$post_title .= " ";
+		$post_title .= ucfirst( $bword );
+		return $post_title;
+	}
+
+
+
+}
