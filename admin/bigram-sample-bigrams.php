af3580db31fc6533843e9c97e23ef8d6a131ecae
Herb Miller <herb@bobbingwide.com>
2024-06-15 15:11:59 +0100

    Hijack bigram sampling logic to test a new implementation #4

--- admin/bigram-sample-bigrams.php
+++ admin/bigram-sample-bigrams.php
@@ -1,31 +1,32 @@
 <?php
 
 /**
- * @copyright (C) Copyright Bobbing Wide 2018
+ * @copyright (C) Copyright Bobbing Wide 2018, 2024
  *
  */
- 
- 
-if ( PHP_SAPI !== "cli" ) { 
+
+
+if ( PHP_SAPI !== "cli" ) {
 	die();
 }
 
 /**
  * Extracts existing bigrams from content
- * 
+ *
  * Implement issue #4 in batch
- * 
+ *
  * 1. List all bigrams
- * 2. Create mapping 
+ * 2. Create mapping
  * 3. For each post in the mapping
  * 4. sample_bigram - find sampled bigrams
  * 5. Create each new sampled bigram
- * 
+ *
  */
 function bigram_sample_bigrams() {
 	oik_require( "classes/class-sample-bigrams.php", "bigram" );
 	$sample_bigrams = new sample_bigrams();
 	$sample_bigrams->set_echo( true );
+
 	$sample_bigrams->get_default_category();
 	$sample_bigrams->load();
 	$sample_bigrams->map_posts();
@@ -35,9 +36,21 @@ function bigram_sample_bigrams() {
 
 }
 
- 
- 
-bigram_sample_bigrams();
+function bigram_sample_post( $post_ID=6550 ) {
+	oik_require( "classes/class-sample-bigrams.php", "bigram" );
+	$sample_bigrams = new sample_bigrams();
+	$sample_bigrams->set_echo();
+	$post = get_post( $post_ID);
+	//print_r( $post );
+	if ( $post ) {
+		$sample_bigrams->sample_post( $post_ID, $post, true );
+	}
+}
+
+
+
+//bigram_sample_bigrams();
+bigram_sample_post();
 
 
 
